ebe0ea0d81224ca78863396b36b997b180519c92
Anhnguyen0812 <23020009@vnu.edu.vn>
2025-11-20 02:28:40 +0700

    4

--- configs/test_config.py
+++ configs/test_config.py
@@ -4,20 +4,20 @@ import numpy as np
 
 IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
 MODEL = 'RefineNetNew'
-DATA_DIRECTORY ='./'
+DATA_DIRECTORY ='/kaggle/input/fifo-dataset'
 DATA_CITY_PATH = './dataset/cityscapes_list/clear_lindau.txt'
-DATA_DIRECTORY_CITY = './leftImg8bit'
-DATA_LIST_PATH_EVAL = './foggy_zurich/Foggy_Zurich/lists_file_names/RGB_testv2_filenames.txt'
+DATA_DIRECTORY_CITY = '/kaggle/input/fifo-dataset/leftImg8bit'
+DATA_LIST_PATH_EVAL = '/kaggle/input/fifo-dataset/foggy_zurich/Foggy_Zurich/lists_file_names/RGB_testv2_filenames.txt'
 DATA_LIST_PATH_EVAL_FD ='./lists_file_names/leftImg8bit_testall_filenames.txt'
 DATA_LIST_PATH_EVAL_FDD ='./lists_file_names/leftImg8bit_testdense_filenames.txt' 
-DATA_DIR_EVAL = './'
-DATA_DIR_EVAL_FD = './Foggy_Driving'
+DATA_DIR_EVAL = '/kaggle/input/fifo-dataset'
+DATA_DIR_EVAL_FD = '/kaggle/input/fifo-dataset/Foggy_Driving'
 NUM_CLASSES = 19 
 RESTORE_FROM = 'no model'
 SNAPSHOT_DIR = f'./snapshots/FIFO'
-GT_DIR_FZ = './foggy_zurich/Foggy_Zurich'
-GT_DIR_FD = './Foggy_Driving'
-GT_DIR_CLINDAU = './gtFine/gtFine'
+GT_DIR_FZ = '/kaggle/input/fifo-dataset/foggy_zurich/Foggy_Zurich'
+GT_DIR_FD = '/kaggle/input/fifo-dataset/Foggy_Driving'
+GT_DIR_CLINDAU = '/kaggle/input/fifo-dataset/gtFine/gtFine'
 SET = 'val'
 
 MODEL = 'RefineNetNew'
@@ -41,7 +41,7 @@ def get_arguments():
     parser.add_argument("--gt-dir-fz", type=str, default=GT_DIR_FZ)
     parser.add_argument("--gt-dir-fd", type=str, default=GT_DIR_FD)
     parser.add_argument("--gt-dir-clindau", type=str, default=GT_DIR_CLINDAU)
-    parser.add_argument("--devkit-dir-fz", default='./foggy_zurich/Foggy_Zurich/lists_file_names') 
+    parser.add_argument("--devkit-dir-fz", default='/kaggle/input/fifo-dataset/foggy_zurich/Foggy_Zurich/lists_file_names') 
     parser.add_argument("--devkit-dir-fd", default='./lists_file_names') 
     parser.add_argument("--devkit-dir-clindau", default='./dataset/cityscapes_list')
     return parser.parse_args()
