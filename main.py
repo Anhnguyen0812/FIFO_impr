3e5697ad5a34f0ea211a64f60531c4a281774ce3
Anhnguyen0812 <23020009@vnu.edu.vn>
2025-11-20 13:55:47 +0700

    4.75

--- main.py
+++ main.py
@@ -382,7 +382,7 @@ def main():
 
                     fogpassfilter.eval()
 
-                    for batch_idx in range(4):
+                    for batch_idx in range(args.batch_size):
                         b_gram = gram_matrix(b_feature[batch_idx])
                         a_gram = gram_matrix(a_feature[batch_idx])
 
@@ -398,7 +398,7 @@ def main():
                         
                         layer_fsm_loss += fsm_weights[layer]*torch.mean((fog_factor_b/(hb*wb) - fog_factor_a/(ha*wa))**2)/half/ b_feature.size(0)
 
-                    loss_fsm += layer_fsm_loss / 4.
+                    loss_fsm += layer_fsm_loss / args.batch_size
 
                 loss = loss_seg_sf + loss_seg_cw + args.lambda_fsm*loss_fsm + args.lambda_con*loss_con  
                 loss = loss / args.iter_size
